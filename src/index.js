59ea10f19055235e31d62cdb900f8f6188b9b4dc
Reagan Nyauma <jreagan2525@gmail.com>
2025-04-09 17:08:18 +0300

    used DOM manipulation to add items to a list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // get relevant target elements
+  const taskForm = document.getElementById("create-task-form");
+  const taskDescription = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (taskDescription.value !== "") {
+      // Create a new task item
+      let newTask = document.createElement("li");
+      newTask.textContent = taskDescription.value;
+
+      // Append the new task to the task list
+      taskList.appendChild(newTask);
+
+      // Clear the input field
+      taskDescription.value = "";
+    }
+  });
 });
